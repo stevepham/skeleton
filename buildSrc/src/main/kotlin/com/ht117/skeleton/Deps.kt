eee98fc79cef7e130c57270ee621c8ab3da087a1
Hy.P <hy.pham@binance.com>
2022-08-23 18:15:50 +0700

    Init project skeleton

---
+++ buildSrc/src/main/kotlin/com/ht117/skeleton/Deps.kt
@@ -0,0 +1,12 @@
+package com.ht117.skeleton
+
+object Deps {
+
+    const val coreKtx = "androidx.core:core-ktx:1.8.0"
+    const val appCompat = "androidx.appcompat:appcompat:1.5.0"
+    const val material = "com.google.android.material:material:1.6.1"
+    const val constraint = "androidx.constraintlayout:constraintlayout:2.1.4"
+
+    const val coCoroutine = "org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4"
+    const val adCoroutine = "org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4"
+}
